ac02b0b09cdc2cc4295a133ed38f13b20da2b305
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-14 02:52:56 +0530

    Update start.py

--- Yumikoo/modules/start.py
+++ Yumikoo/modules/start.py
@@ -88,8 +88,8 @@ async def start(client: Client, message: Message):
                 InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
             ],
             [
-                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/iam_daxx"),
-                InlineKeyboardButton("ᴍᴀɪɴᴛᴀɪɴᴇʀ", url="https://t.me/herokufreecc"),
+                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/herokufreecc"),
+                InlineKeyboardButton("ᴍᴀɪɴᴛᴀɪɴᴇʀ", url="https://t.me/iam_daxx"),
             ],
             [
                 InlineKeyboardButton("ᴄᴏᴍᴍᴀɴᴅs", callback_data="help_")
