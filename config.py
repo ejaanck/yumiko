306e1e71f44e89adf90da609078bb52284c07312
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-18 21:06:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "6435225"))
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
 BOT_USERNAME = getenv("BOT_USERNAME", "YumikooBot")
 COMMAND_HANDLER = ["/", "!"]
-BOT_TOKEN = getenv("BOT_TOKEN", "6196153811:AAET3ADh9AtURnKZkvCNENc6mfG_8Xt-sQo")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6691393517").split()))
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority")
-SESSION_STRING = getenv("SESSION_STRING", "BABiMZkAbe5o0nxPRivuf7xl0MU35q7XFPsMDcylv3I52WZyA4jjmZ7YrcyD_Wo3NGcCZ3nltdcb5SCAim-6LJ1rrQ9rPuEtLwlEwRihToKYkTFW9FZjCcdA7flVrtk9dQPpnDIWI_6ZaEQn1QhfI-SlBOiuYJ1ucoEQF2vOj1blvratq9-JGEtYDr8eA7o1Q3_neX82fDBAiBruh11ed6KpWiBgtKm9NERUZPvhvWbhSyODnI1QLbKx9Rd9wjn4CpS27TayfxgFQCHfpYAeLFAYGm0I6LPGYc6i0E1OOpD5Aq8cz-VYIJZylSu5kDf-m1quGBCBOxsQ9GObyxmmdDym0ZvMMQAAAAFRHMccAA")
+MONGO_URL = getenv("MONGO_URL", "")
+SESSION_STRING = getenv("SESSION_STRING", "")
