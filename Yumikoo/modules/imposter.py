95d0c1c88ae5e2fa03a96a0b7736dce3722e176d
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-18 14:38:45 +0530

    Update imposter.py

--- Yumikoo/modules/imposter.py
+++ Yumikoo/modules/imposter.py
@@ -86,7 +86,7 @@ async def chk_usr(_, message: Message):
             message.from_user.last_name,
         )
     if msg != "":
-        await message.reply_photo("https://graph.org//file/a5f944533dcaccfaf2567.jpg", caption=msg)
+        await message.reply_photo("https://telegra.ph/file/9ef592145fd4ea189d092.jpg", caption=msg)
 
 
 @Yumikoo.on_message(filters.group & filters.command("imposter") & ~filters.bot & ~filters.via_bot)
